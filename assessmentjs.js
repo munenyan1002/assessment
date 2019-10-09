e288ebdb151b2291279a1577188fb6b6b16ad339
munenyan1002 <32436088+munenyan1002@users.noreply.github.com>
2019-10-10 04:15:39 +0900

    Create assessmentjs.js

---
+++ assessmentjs.js
@@ -0,0 +1,12 @@
+(function () {
+  'use strict';
+  const userNameInput = document.getElementById('user-name');
+  const assessmentButton = document.getElementById('assessment');
+  const resultDivided = document.getElementById('result-area');
+  const tweetDivided = document.getElementById('tweet-area';
+  
+  )
+}
+
+  
+)
